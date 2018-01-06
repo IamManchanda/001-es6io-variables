8f7d50516dfab772f7a3623b335c4d0e8fdb7eb0
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-01-06 12:00:28 +0530

    Temporal Dead Zone!

--- js/script.js
+++ js/script.js
@@ -2,14 +2,5 @@
  * JavaScript
  */
 
-{
-  const name = 'harry';
-  console.log(name);
-};
-
-for (let i = 1; i <= 10; i++) {
-  console.log(i);
-  setTimeout(() => {
-    console.log(`The Number is ${i }`);
-  }, 1000)
-}
+console.log(pizza);
+const pizza = 'Deep Dish 🍕🍕🍕';
